be9755e57866a9b4cd7ecb2bfeda96ca22b27628
mcupino@uci.edu <mcupino@uci.edu@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-01-04 18:39:57 +0000

    Issue 5508044: Making app calls to a user's Google Docs List

--- helloworld.py
+++ helloworld.py
@@ -4,8 +4,9 @@ import urllib
 import webapp2
 import jinja2
 import os
-import gdata.docs.data
+import gdata.gauth
 import gdata.docs.client
+import ConfigParser
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -13,6 +14,63 @@ jinja_environment = jinja2.Environment(
 from google.appengine.ext import db
 from google.appengine.api import users
 
+from google.appengine.ext.webapp.util import run_wsgi_app, login_required
+
+# Configure gdata
+config = ConfigParser.RawConfigParser()
+config.read('config.cfg')
+SETTINGS = {
+    'APP_NAME': config.get('gdata_settings', 'APP_NAME'),
+    'CONSUMER_KEY': config.get('gdata_settings', 'CONSUMER_KEY'),
+    'CONSUMER_SECRET': config.get('gdata_settings', 'CONSUMER_SECRET'),
+    'SCOPES': [config.get('gdata_settings', 'SCOPES')]
+    }
+
+gdocs = gdata.docs.client.DocsClient(source = SETTINGS['APP_NAME'])
+
+class Fetcher(webapp2.RequestHandler):
+
+    @login_required
+    def get(self):
+        current_user = users.get_current_user()
+
+        scopes = SETTINGS['SCOPES']
+        oauth_callback = 'http://%s/step2' % self.request.host
+        consumer_key = SETTINGS['CONSUMER_KEY']
+        consumer_secret = SETTINGS['CONSUMER_SECRET']
+        request_token = gdocs.get_oauth_token(scopes, oauth_callback,
+            consumer_key, consumer_secret)
+
+        request_token_key = 'request_token_%s' % current_user.user_id()
+        gdata.gauth.ae_save(request_token, request_token_key)
+
+        approval_page_url = request_token.generate_authorization_url()
+
+        message = """<a href="%s">
+        Request token for the Google Documents Scope</a>"""
+        self.response.out.write(message % approval_page_url)
+
+
+class RequestTokenCallback(webapp2.RequestHandler):
+
+    @login_required
+    def get(self):
+        current_user = users.get_current_user()
+
+        request_token_key = 'request_token_%s' % current_user.user_id()
+        request_token = gdata.gauth.ae_load(request_token_key)
+        gdata.gauth.authorize_request_token(request_token, self.request.uri)
+
+        gdocs.auth_token = gdocs.get_access_token(request_token)
+
+        access_token_key = 'access_token_%s' % current_user.user_id()
+        gdata.gauth.ae_save(request_token, access_token_key)
+
+        # gdocs.GetDocList() is deprecated; gdocs.GetResources() is the new way
+        feed = gdocs.GetResources()
+        for entry in feed.entry:
+            template = '<div>%s</div>'
+            self.response.out.write(template % entry.title.text)
 
 class Greeting(db.Model):
   """Models an individual Guestbook entry with an author, content, and date."""
@@ -75,5 +133,7 @@ class GoogleWebmasterVerify(webapp2.RequestHandler):
 # automatically route to the html page?
 app = webapp2.WSGIApplication([('/', MainPage),
     ('/sign', Guestbook),
-    ('/google910e6da758dc80f1.html', GoogleWebmasterVerify)],
+    ('/google910e6da758dc80f1.html', GoogleWebmasterVerify),
+    ('/step1', Fetcher),
+    ('/step2', RequestTokenCallback)],
     debug=True)
