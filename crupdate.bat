911b4e6c21a1812932fb3065a093dcc13b8b3d79
daniellekyu@gmail.com <daniellekyu@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-01-11 05:56:28 +0000

    Issue 5530077: Removing crupload from codebase

--- crupdate.bat
+++
@@ -1,2 +0,0 @@
-set /p issueNumber=What is the code review number you want to update?
-"C:\Program Files\codereview\upload.py" --issue %issueNumber% --send_mail --server codereview.appspot.com
--- crupload.bat
+++
@@ -1,2 +0,0 @@
-set /p reviewer=Who would you like to review your code?
-"C:\Program Files\codereview\upload.py" --cc sucy-uci-code-reviews@googlegroups.com --reviewers %reviewer% --send_mail --server codereview.appspot.com
