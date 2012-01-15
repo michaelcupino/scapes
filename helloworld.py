2c11ab7c249166d2293d28c6a33fa03cb36c71cf
jordan.a.speer@gmail.com <jordan.a.speer@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-01-15 07:16:35 +0000

    Quick fix to step4

--- helloworld.py
+++ helloworld.py
@@ -79,7 +79,7 @@ class RequestRevision(webapp2.RequestHandler):
         self.response.out.write("<a href='/step1'>Step 1</a> <br \>")
         
         feed = gdocs.GetResources()
-        doc = feed.entry[id]
+        doc = feed.entry[int(id)]
         revisions = gdocs.GetRevisions(doc)
                  
         count = 1;
