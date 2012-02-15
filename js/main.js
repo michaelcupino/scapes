99b7cded7aa2460d84b63200281fae26479a5d3f
mcupino@uci.edu <mcupino@uci.edu@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-02-15 09:22:30 +0000

    Issue 5673054: Added POST ajax example

--- js/main.js
+++ js/main.js
@@ -15,56 +15,14 @@ function onAddSuccess(response) {
 }
 
 function doAdd() {
-  server.Add(document.getElementById('num1').value,
-      document.getElementById('num2').value,
-      onAddSuccess);
+  $.ajax({
+    type: "POST",
+    url: "/rpc",
+    data: {
+      action : "Add",
+      arg0 : $("#num1").val(),
+      arg1 : $("#num2").val()
+    },
+    success: onAddSuccess
+  });
 }
-
-function Request(function_name, opt_argv) {
-  if(!opt_argv) {
-    opt_argv = new Array();
-  }
-
-  var callback = null;
-  var len = opt_argv.length;
-  if (len > 0 && typeof opt_argv[len-1] == 'function') {
-    callback = opt_argv[len-1];
-    opt_argv.length--;
-  }
-  var async = (callback != null);
-
-  var query = 'action=' + encodeURIComponent(function_name);
-  for (var i = 0; i < opt_argv.length; i++) {
-    var key = 'arg' + i;
-    var val = JSON.stringify(opt_argv[i]);
-    query += '&' + key + '=' + encodeURIComponent(val);
-  }
-
-  var req = new XMLHttpRequest();
-  req.open('GET', '/rpc?' + query, async);
-
-  if (async) {
-    req.onreadystatechange = function() {
-      if (req.readyState == 4 && req.status == 200) {
-        var response = null;
-        try {
-          response = JSON.parse(req.responseText);
-        } catch (e) {
-          response = req.responseText;
-        }
-        callback(response);
-      }
-    }
-  }
-
-  req.send(null);
-}
-
-function InstallFunction(obj, name) {
-  obj[name] = function() {
-    Request(name, arguments);
-  }
-}
-
-var server = {};
-InstallFunction(server, 'Add');
