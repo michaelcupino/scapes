70d4b5956d6151a6ab7fca6034b8bcfad1a75286
jordan.a.speer@gmail.com <jordan.a.speer@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-03-15 19:25:53 +0000

    Refactor (issue 5822051)

--- trunk/test/test_main.py
+++ trunk/test/test_main.py
@@ -1,24 +1,32 @@
 import unittest
-import main
+import difflib
+import diff_match_patch.diff_match_patch
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from google.appengine.ext import testbed
+from main import RequestRevision
+
+
+gdiff = diff_match_patch.diff_match_patch()
 
 # http://docs.python.org/library/test.html#writing-tests
 # http://code.google.com/p/gaeunit/wiki/Readme
 # http://code.google.com/appengine/docs/python/tools/localunittesting.html#Writing_Datastore_and_Memcache_Tests
 
+class TestRevisionModel(db.Model):
+  resourceLink = db.StringProperty()
+  revisionNumber = db.StringProperty()
+  revisionDownloadedText = db.BlobProperty()
+
 class MainTest(unittest.TestCase):
 
   def setUp(self):
-    y = 0
+    self.requestObject = RequestRevision()
 
-  def tearDown(self):
-    y = None
+  def testIsRemove(self):
+    x = self.requestObject.isRemove([gdiff.DIFF_DELETE, "STUFF DELETED HERE"])
+    self.assertTrue(x)
+    y = self.requestObject.isRemove([gdiff.DIFF_INSERT, "STUFF ADDED HERE"])
+    self.assertFalse(y)
 
-  def testFunction1(self):
-    x = 0
-    self.assertEqual(0, x)
-    self.assertTrue(True)
-
-  def testAwesomeness2(self):
-    x = 0
-    self.assertEqual(0, x)
-    self.assertTrue(True)
