addf1a65312da33c8221529523e45db38f8c181f
mcupino@uci.edu <mcupino@uci.edu@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-06-08 08:52:59 +0000

    Issue 6308052: Added analyzer stubs

---
+++ trunk/gdocumentanalyzer.py
@@ -0,0 +1,10 @@
+from analyzer import Analyzer
+
+class GDocumentAnalyzer(Analyzer):
+  """GDocumentAnalyzer contains the logic to analyze a document"""
+
+  def __init__(self):
+    """Initializes the object"""
+
+    print "GDocumentAnalyzer.__init__()"
+    pass
---
+++ trunk/grevisionanalyzer.py
@@ -0,0 +1,10 @@
+from analyzer import Analyzer
+
+class GRevisionAnalyzer(Analyzer):
+  """GRevisionAnalyzer contains the logic to analyze a revision"""
+
+  def __init__(self):
+    """Initializes the object"""
+
+    print "GRevisionAnalyzer.__init__()"
+    pass
