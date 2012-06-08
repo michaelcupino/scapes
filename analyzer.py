81ef97075151d9ffc6a396c8f0f663a95b8294d5
mcupino@uci.edu <mcupino@uci.edu@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2012-06-08 08:52:59 +0000

    Issue 6308052: Added analyzer stubs

---
+++ analyzer.py
@@ -0,0 +1,8 @@
+class Analyzer():
+  """Analyzer is the parent class"""
+
+  def __init__(self):
+    """Initializes the object"""
+
+    print "Analyzer.__init__()"
+    pass
---
+++ gfolderanalyzer.py
@@ -0,0 +1,10 @@
+from analyzer import Analyzer
+
+class GFolderAnalyzer(Analyzer):
+  """GFolderAnalyzer contains the logic to analyze a folder"""
+
+  def __init__(self):
+    """Initializes the object"""
+
+    print "GFolderAnalyzer.__init__()"
+    pass
