848dfc7c01a9d7c2e3cbb228b4dea1d9e2e3eb98
PythonNut <PythonNut@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2013-11-11 04:24:29 +0000

    Further work on 59414582, hopefully I can get this fixed.

--- trunk/drive_appengine/handler/revision_handler.py
+++ trunk/drive_appengine/handler/revision_handler.py
@@ -4,7 +4,7 @@ class RevisionHandler(webapp2.RequestHandler):
   """The RevisionHandler handles revision requests and gives a response with a
   list of revisions for a specific document.
   """
-
+  
   def get(self):
     """Gets the list of revisions"""
     # TODO(PythonNut): Write the list of revisions in the response.
--- trunk/drive_appengine/scapes_test.py
+++ trunk/drive_appengine/scapes_test.py
@@ -1,30 +1,31 @@
 #/usr/bin/env python
 import httplib2
 import pprint
-
+import webbrowser
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
 
-# Copy your credentials from the console
-CLIENT_ID = '551548476927-h19sqjvvej0du0f6a487u0iac6u3dh7k.apps.googleusercontent.com'
-CLIENT_SECRET = 'Lu7PouJg0QSxYzaWoJUl_eqD'
+with open("client_secrets.json") as f:
+    import json
+    data          = json.loads(f.read())
+    CLIENT_ID     = data["web"]["client_id"]
+    CLIENT_SECRET = data["web"]["client_secret"]
 
 # Check https://developers.google.com/drive/scopes for all available scopes
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 
 # Redirect URI for installed apps
-REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
-# Path to the file to upload
-FILENAME = 'document.txt'
+# REDIRECT_URI = 'http://localhost:8080/oauth2callback'
+REDIRECT_URI = 'http://localhost:8080/oauth2callback'
 
 # Run through the OAuth flow and retrieve credentials
 flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
 authorize_url = flow.step1_get_authorize_url()
+webbrowser.open(authorize_url)
 print 'Go to the following link in your browser: ' + authorize_url
+
 code = raw_input('Enter verification code: ').strip()
-#code = "4/1lDr85ZH4CnHTofVYh3AXb5TWw5Y.MoHnu-2zyIYbmmS0T3UFEsOGgyWJhAI"
 credentials = flow.step2_exchange(code)
 
 # Create an httplib2.Http object and authorize it with our credentials
