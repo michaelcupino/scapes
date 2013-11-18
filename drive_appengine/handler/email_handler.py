723a097aa929596277f04d551bc9fa86817de829
tbawaz@gmail.com <tbawaz@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2013-11-18 09:54:14 +0000

    Added CSV functionality

--- drive_appengine/handler/email_handler.py
+++ drive_appengine/handler/email_handler.py
@@ -1,4 +1,6 @@
 import webapp2
+import csv
+import tempfile
 from google.appengine.api import mail
 
 class EmailHandler(webapp2.RequestHandler):
@@ -6,15 +8,29 @@ class EmailHandler(webapp2.RequestHandler):
   scapes robot account.
   """
   
+
+
   def get(self):
     """Sends an email."""
-    message = mail.EmailMessage(
-        sender='Scapes Robot <robot@scapes-uci.appspotmail.com>',
-        subject='Getting through the first sprint')
 
-    message.to = 'Tristan Biles <tbawaz@gmail.com>'
-    message.body = 'Dear Tristan:\n\n' + 'Finally getting this to work!'
+    # TODO(tbawaz): Send a hardcoded email.
+	
+    
+    csvFile = tempfile.TemporaryFile()
+    writer = csv.writer(csvFile)
+    values = [['Date', 'Time']]
+    writer.writerows(values)
+    csvFile.seek(0)
+    csvFileBytes = csvFile.read()  # <--- this is what you're looking for
+    csvFile.close()
+	
+    mail.send_mail("Scapes Robot <robot@scapes-uci.appspotmail.com>",
+              to="Tristan Biles <tbawaz@gmail.com>",
+              subject="Getting through the first sprint",
+              body="""
+	Testing the mail.send_mail() function over the mail.EmailMessage()
+	""", attachments = [("csvTempFile",csvFileBytes)])
+    
+    self.response.write('Email has been successfully sent. Making sure CSV1 sends as well')
 
-    message.send()
-    self.response.write('Details of the email message will be here. Hopefully '
-        'the email will have been sent!')
+    
