fe37b4c33ea331228be5251e5c704909cf69ce58
tbawaz@gmail.com <tbawaz@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2013-12-19 00:55:22 +0000

    Debugging CSV prototype

--- trunk/handler/email_handler.py
+++ trunk/handler/email_handler.py
@@ -1,6 +1,7 @@
 import webapp2
 import csv
 import tempfile
+import logging
 from google.appengine.api import mail
 
 class EmailHandler(webapp2.RequestHandler):
@@ -16,21 +17,21 @@ class EmailHandler(webapp2.RequestHandler):
     # TODO(tbawaz): Send a hardcoded email.
 	
     
-    csvFile = tempfile.TemporaryFile()
-    writer = csv.writer(csvFile)
+    csvFile = tempfile.TemporaryFile() #makes a temp file
+    writer = csv.writer(csvFile) # makes the temp file a csv file
     values = [['Date', 'Time']]
     writer.writerows(values)
     csvFile.seek(0)
     csvFileBytes = csvFile.read()  # <--- this is what you're looking for
     csvFile.close()
 	
-    mail.send_mail("Scapes Robot <robot@scapes-uci.appspotmail.com>",
-              to="Tristan Biles <tbawaz@gmail.com>",
-              subject="Getting through the first sprint",
-              body="""
-	Testing the mail.send_mail() function over the mail.EmailMessage()
-	""", attachments = [("csvTempFile",csvFileBytes)])
+    mail.send_mail(sender = "Scapes Robot <robot@scapes-uci.appspotmail.com>",
+              to = "Tristan Biles <tbawaz@gmail.com>",
+              subject = "Getting through the first sprint",
+              body = """
+				Testing the mail.send_mail() function over the mail.EmailMessage()
+				""", 
+				attachments = [("csvTempFilee",csvFileBytes)])
     
     self.response.write('Email has been successfully sent. Making sure CSV1 sends as well')
-
-    
+    logging.info("value of my csv is %s", csvFileBytes)
