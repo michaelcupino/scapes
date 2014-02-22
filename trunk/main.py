393c18b62b17c80c19c58d7c426d24dea500177f
tbawaz@gmail.com <tbawaz@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-02-22 19:09:18 +0000

    committing main.py

--- trunk/main.py
+++ trunk/main.py
@@ -38,7 +38,6 @@ from handler.email_handler import EmailHandler
 from handler.revision_handler import RevisionHandler
 from handler.file_id_handler import FileIDHandler
 from handler.map_reduce_handler import MREmailHandler
-from handler.mr_demo_handler import MRDemoHandler
 from handler.wordcount_handler import WordcountHandler, UploadHandler, ServeHandler
 from handler.hello_world_handler import HelloWorldHandler
 
@@ -120,7 +119,6 @@ app = webapp2.WSGIApplication(
      ('/revisions', RevisionHandler),
      ('/fileids',FileIDHandler),
      ('/MREmail',MREmailHandler),
-     ('/demo', MRDemoHandler),
      ('/wordcount',WordcountHandler),
      ('/serve/([^/]+)?',ServeHandler),
      ('/upload',UploadHandler),
