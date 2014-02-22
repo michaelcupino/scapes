f4a1384cb4ba1ee4383777a566f1c291e12bc739
tbawaz@gmail.com <tbawaz@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-02-22 18:46:30 +0000

    committed map_reduce_handler.py

--- trunk/handler/map_reduce_handler.py
+++ trunk/handler/map_reduce_handler.py
@@ -1,168 +1,148 @@
-# #All MapReduce function goes here
-#  
-# import datetime
-# import jinja2
-# import logging
-# import re
-# import urllib
-# import webapp2
-#  
-# from google.appengine.ext import blobstore
-# from google.appengine.ext import db
-#  
-# from google.appengine.ext.webapp import blobstore_handlers
-#  
-# from google.appengine.api import files
-# from google.appengine.api import taskqueue
-# from google.appengine.api import users
-# from google.appengine.ext import blobstore
-#  
-# from mapreduceDemo.mapreduce import base_handler
-# from mapreduceDemo.mapreduce import mapreduce_pipeline
-# from mapreduceDemo.mapreduce import operation as op
-# from mapreduceDemo.mapreduce import shuffler
-# from handler.email_handler import EmailHandler
-# from service import config
-#  
-# # import scapes_revision_drive is this necessary?
-#  
-# """
-#  
-# Whoever is doing the template for homepage got to edit IndexHandler Class. Note-
-# 1. Attach the getFirstKeyForUser() and getLastKeyForUser() helper methods (check example)
-# 2. Attach FileMetaData class if needed
-# 3. Load correct template
-# 4. Change post method accordingly
-#  
-# class IndexHandler(webapp2.RequestHandler):
-#   The main page that users will interact with, which presents users with                                                                           
-#   the ability to upload new data or run MapReduce jobs on their existing data.                                                                        
-#    
-#  
-#   template_env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"),
-#                                     autoescape=True)
-#  
-#   def get(self):
-#     user = users.get_current_user()
-#     username = user.nickname()
-#  
-#  
-#     first = FileMetadata.getFirstKeyForUser(username)
-#     last = FileMetadata.getLastKeyForUser(username)
-#  
-#     q = FileMetadata.all()
-#     q.filter("__key__ >", first)
-#     q.filter("__key__ < ", last)
-#     results = q.fetch(10)
-#  
-#     items = [result for result in results]
-#     length = len(items)
-#  
-#     upload_url = blobstore.create_upload_url("/upload")
-#  
-#     self.response.out.write(self.template_env.get_template("index.html").render(
-#         {"username": username,
-#          "items": items,
-#          "length": length,
-#          "upload_url": upload_url}))
-#  
-#   def post(self):
-#     blob_key = self.request.get("blobkey")
-#     filekey = self.request.get("filekey")
-#  
-#     if self.request.get("word_count"):
-#       pipeline = WordCountPipeline(filekey, blob_key)
-#     elif self.request.get("index"):
-#       pipeline = IndexPipeline(filekey, blob_key)
-#     else:
-#       pipeline = PhrasesPipeline(filekey, blob_key)
-#  
-#     pipeline.start()
-#     self.redirect(pipeline.base_path + "/status?root=" + pipeline.pipeline_id)
-# """
-#  
-#  
-#      
-# def send_email_map(data):
-#     """Send email map function"""
-#     raise TypeError()
-#     return data
-#     pass
-#      
-# def send_email_reduce(key,values):
-#     """Send email reduce function"""
-#     # yield
-#     raise ValueError()
-#     EmailHandler.run()
-#     pass
-#      
-# class SendEmailPipeline(base_handler.PipelineBase):
-#     """A pipeline to run Send email demo. """
-#     def run(self):
-#          
-#         yield mapreduce_pipeline.MapreducePipeline(
-#             "send_email",
-#             "main.send_email_map",
-#             "main.send_email_reduce",
-#             "mapreduce.input_readers.DatastoreInputReader",
-#             "mapreduce.output_readers.FileOutputWriter",
-#             mapper_params={
-#             # "blob_key": blobkey,
-#         },
-#         reducer_params={
-#             # "mime_type": "text/plain",
-#             # "output_writer": {
-#             #     "mime_type": "text/plain",
-#             #     "output_sharding": "input",
-#             #     "filesystem": "blobstore",
-#             # },
-#  
-#         },
-#         shards=16)
-#  
-# class MREmailHandler(webapp2.RequestHandler):
-#     def get(self, *args):
-#         global pipeline
-#         print(">"*100)
-#         print(args)
-#         print("<"*100)
-#         print("="*99)
-#          
-#          
-#      
-#  
-#     def post(self,*args):
-#         print(">"*100)
-#         print(args)
-#         print("<"*100)
-#         pipeline = SendEmailPipeline()
-#         pipeline.start()
-#         
-#  
-#          
-# def revision_map(file_id):
-#   http = config.decorator.http()
-#   for revisions in retrieve_revisions(http, file_id):
-#       yield (file_id, "")
-#  
-# def revision_reduce(key, values):
-#   yield "%s: %d\n" % (key, len(values))
-#  
-# class RevisionCountPipeline(base_handler.PipelineBase):
-#   def run(self, filekey, blobkey):
-#     logging.debug("filename is %s" % filekey)
-#     output = yield mapreduce_pipeline.MapreducePipeline(
-#         "revision_count",
-#         "map_reduce_handler.revision_map",
-#         "map_reduce_handler.revision_reduce",
-#   #      "mapreduce.input_readers.BlobstoreZipInputReader", Got to fix this accordingly
-#   #      "mapreduce.output_writers.BlobstoreOutputWriter", Got to fix this accordingly
-#         mapper_params={
-#             "blob_key": blobkey,
-#         },
-#         reducer_params={
-#             "mime_type": "text/plain",
-#         },
-#         shards=16)
-#     self.response.write('Email has been successfully sent using a mapreduce')
-#     yield StoreOutput("RevisionCount", filekey, output)
+import webapp2
+from service import config
+import file_core
+import revision_core
+import os
+import urllib
+import logging
+from mapreduce_dependencies import input_readers
+from mapreduce_dependencies import output_writers
+from mapreduce_dependencies import mapreduce_pipeline
+from mapreduce_dependencies import base_handler
+from email_handler import EmailHandler
+
+from google.appengine.ext import blobstore,db
+from google.appengine.ext.webapp import blobstore_handlers
+
+class MRSendEmailPipeline(base_handler.PipelineBase):
+  """A pipeline to run Word count demo.
+
+  Args:
+    blobkey: blobkey to process as string. Should be a zip archive with
+      text files inside.
+  """
+  print('Entered Pipeline')
+  def run(self, filekey, blobkey):
+    print('WORDCOUNTPIPELINEWORDCOUNTPIPELINEWORDCOUNTPIPELINEWORDCOUNTPIPELINEWORDCOUNTPIPELINE')
+    print('filekey is {}'.format(filekey))
+    output = yield mapreduce_pipeline.MapreducePipeline(
+        "send_email",
+        "send_email_map",
+        "send_email_reduce",
+        "mapreduce_dependencies.input_readers.BlobstoreZipInputReader",
+        "mapreduce_dependencies.output_writers.BlobstoreOutputWriter",
+        mapper_params={
+            "blob_key": blobkey,
+        },
+        reducer_params={
+            "mime_type": "text/plain",
+        },
+        shards=16)
+    yield StoreOutput("MRSendEmail", filekey, output)
+    
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  print("Entered UploadHandlers")
+  def post(self):
+      print("Entered POST of Up")
+      upload_files = self.get_uploads('file')
+      print(upload_files)
+      if len(upload_files) > 0:
+        blob_info = upload_files[0]
+        blob_key = blob_info.key()
+        Wrapper(blob=blob_key).put()
+        blob_key = self.request.get('blobkey')
+        filekey = self.request.get('filekey')
+        pipeline = MRSendEmailPipeline(filekey,blob_key)
+        logging.debu("got to pipeline start")
+        print("got to pipeline.start")
+        pipeline.start()
+        self.redirect('/serve/{}'.format(blob_info.key()))
+        self.response.out.write('Uploading disabled')
+        pipeline.start()
+
+class Wrapper(db.Model):
+  print("Entered Wrapper")
+  user = db.UserProperty(auto_current_user=True)
+  blob = blobstore.BlobReferenceProperty(required=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  print("Entered ServeHanlder")
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
+
+        
+
+class StoreOutput(base_handler.PipelineBase):
+  """A pipeline to store the result of the MapReduce job in the database.
+
+  Args:
+    mr_type: the type of mapreduce job run (e.g., WordCount, Index)
+    encoded_key: the DB key corresponding to the metadata of this job
+    output: the blobstore location where the output of the job is stored
+  """
+  print("Entered StoreOutput")
+  def run(self, mr_type, encoded_key, output):
+    logging.debug("output is %s" % str(output))
+    key = db.Key(encoded=encoded_key)
+    m = FileMetadata.get(key)
+
+    if mr_type == "MRSendEmail":
+      m.MRSendEmail_link = output[0]
+    elif mr_type == "Index":
+      m.index_link = output[0]
+    elif mr_type == "Phrases":
+      m.phrases_link = output[0]
+
+    m.put()
+
+MAIN_PAGE_HTML = '''\
+<html>
+    <body>
+        <form action = "%s" method = 'POST' enctype = 'mutipart/form-data'>
+
+        <input type="button" value="Click me" onclick="msg()">
+        # TODO: HAVE onclick = ____ send out the email
+        </form>
+    </body>
+</html>
+
+'''
+
+class MREmailHandler(webapp2.RequestHandler):
+    def get(self):
+        print('Entered Get MREmailHandler')
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write(MAIN_PAGE_HTML)
+#         self.response.out.write('<html><body>')
+#         self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+#         self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+#             name="submit" value="Submit"> </form></body></html>""")
+    def post(self):
+        print('Entered Post MREmailHandler')
+
+
+
+###MAP FUNCTION###
+def send_email_map():
+    #maybe need input data?
+  """Word count map function."""
+  print("Entered send map function")
+  EmailHandler.run()
+   
+   
+###REDUCE FUNCTION###    
+def send_email_reduce(key, values):
+  """Word count reduce function."""
+  print("Entered send reduce function")
+ 
+  
+ 
+
+        
+        
+
+  
