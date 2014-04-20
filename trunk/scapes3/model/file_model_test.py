b62adbed1916e29eb14ea7ba13baa56de6b19a7b
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-20 15:17:14 +0000

    Add placeholder File unit tests and add a test_runner to a run multiple app engine tests

---
+++ trunk/scapes3/model/file_model_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import unittest
+
+class FileModelTest(unittest.TestCase):
+
+  def testSomething(self):
+    self.assertEqual(1, 1 + 0)
+
+if __name__ == '__main__':
+  unittest.main()
