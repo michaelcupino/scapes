570fad48b0ff02046a3c90ccb3137a52c95d59e3
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-20 20:36:06 +0000

    Import File module in file_model_test

--- scapes3/model/file_model_test.py
+++ scapes3/model/file_model_test.py
@@ -2,18 +2,31 @@
 import unittest
 
 from google.appengine.ext import testbed
+from google.appengine.api import users
+
+from file_model import File
 
 class FileModelTest(unittest.TestCase):
 
   def setUp(self):
     self.testbed = testbed.Testbed()
     self.testbed.activate()
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_memcache_stub()
 
   def tearDown(self):
     self.testbed.deactivate()
 
   def testSomething(self):
     self.assertEqual(1, 1 + 0)
+
+  def testPut(self):
+    testFile = File()
+    testFile.author = users.get_current_user()
+    testFile.put()
+    # TODO(michaelcupino): Assert that the author that was stored is the same
+    # as the current user.
+    self.assertEqual(2, 1 + 1)
 
 if __name__ == '__main__':
   unittest.main()
