69ebf4aba710f99d28e0f8ec6f9545ded49fde91
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-20 15:31:19 +0000

    Specify pattern for test files in test runner. Add testbed in file_model_test.py

--- scapes3/model/file_model_test.py
+++ scapes3/model/file_model_test.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
 import unittest
 
+from google.appengine.ext import testbed
+
 class FileModelTest(unittest.TestCase):
+
+  def setUp(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+
+  def tearDown(self):
+    self.testbed.deactivate()
 
   def testSomething(self):
     self.assertEqual(1, 1 + 0)
