63f375e2f5953b7c467f2c3d23ca509857456db4
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-21 04:38:57 +0000

    Refcator out FileMetadata model class by moving it to the model package and adding placeholders for unit tests.

---
+++ scapes3/model/file_metadata.py
@@ -0,0 +1,80 @@
+from google.appengine.ext import db
+
+class FileMetadata(db.Model):
+  """A helper class that will hold metadata for the user's blobs.
+
+  Specifially, we want to keep track of who uploaded it, where they uploaded it
+  from (right now they can only upload from their computer, but in the future
+  urlfetch would be nice to add), and links to the results of their MR jobs. To
+  enable our querying to scan over our input data, we store keys in the form
+  'user/date/blob_key', where 'user' is the given user's e-mail address, 'date'
+  is the date and time that they uploaded the item on, and 'blob_key'
+  indicates the location in the Blobstore that the item can be found at. '/'
+  is not the actual separator between these values - we use '..' since it is
+  an illegal set of characters for an e-mail address to contain.
+  """
+
+  __SEP = ".."
+  __NEXT = "./"
+
+  owner = db.UserProperty()
+  filename = db.StringProperty()
+  uploadedOn = db.DateTimeProperty()
+  source = db.StringProperty()
+  blobkey = db.StringProperty()
+  wordcount_link = db.StringProperty()
+  index_link = db.StringProperty()
+  phrases_link = db.StringProperty()
+
+  @staticmethod
+  def getFirstKeyForUser(username):
+    """Helper function that returns the first possible key a user could own.
+
+    This is useful for table scanning, in conjunction with getLastKeyForUser.
+
+    Args:
+      username: The given user's e-mail address.
+    Returns:
+      The internal key representing the earliest possible key that a user could
+      own (although the value of this key is not able to be used for actual
+      user data).
+    """
+
+    return db.Key.from_path("FileMetadata", username + FileMetadata.__SEP)
+
+  @staticmethod
+  def getLastKeyForUser(username):
+    """Helper function that returns the last possible key a user could own.
+
+    This is useful for table scanning, in conjunction with getFirstKeyForUser.
+
+    Args:
+      username: The given user's e-mail address.
+    Returns:
+      The internal key representing the last possible key that a user could
+      own (although the value of this key is not able to be used for actual
+      user data).
+    """
+
+    return db.Key.from_path("FileMetadata", username + FileMetadata.__NEXT)
+
+  @staticmethod
+  def getKeyName(username, date, blob_key):
+    """Returns the internal key for a particular item in the database.
+
+    Our items are stored with keys of the form 'user/date/blob_key' ('/' is
+    not the real separator, but __SEP is).
+
+    Args:
+      username: The given user's e-mail address.
+      date: A datetime object representing the date and time that an input
+        file was uploaded to this app.
+      blob_key: The blob key corresponding to the location of the input file
+        in the Blobstore.
+    Returns:
+      The internal key for the item specified by (username, date, blob_key).
+    """
+
+    sep = FileMetadata.__SEP
+    return str(username + sep + str(date) + sep + blob_key)
+
---
+++ scapes3/model/file_metadata_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import unittest
+
+from datetime import datetime
+from google.appengine.ext import testbed
+from google.appengine.api import users
+from model.file_metadata import FileMetadata
+
+class FileModelTest(unittest.TestCase):
+
+  def setUp(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_memcache_stub()
+
+  def tearDown(self):
+    self.testbed.deactivate()
+
+  def testGetFirstKeyForUser(self):
+    # TODO(michaelcupino): Find out if scapes is using getFirstKeyForUser
+    self.assertEqual(1, 1 + 0)
+
+  def testGetLastKeyForUser(self):
+    # TODO(michaelcupino): Find out if scapes is using getLastKeyForUser
+    self.assertEqual(2, 1 + 1)
+
+  def testGetKeyName(self):
+    # TODO(michaelcupino): Find out if scapes is using getKeyName
+    self.assertEqual(3, 1 + 2)
+
+  def testOwner(self):
+    # TODO(michaelcupino): Find out if scapes is using owner property
+    self.assertEqual(4, 1 + 3)
+
+  def testFilename(self):
+    # TODO(michaelcupino): Find out if scapes is using filename property
+    self.assertEqual(5, 1 + 4)
+
+  def testUploadedOn(self):
+    # TODO(michaelcupino): Find out if scapes is using uploadedOn property
+    self.assertEqual(5, 1 + 4)
+
+  def testSource(self):
+    # TODO(michaelcupino): Find out if scapes is using source property
+    self.assertEqual(5, 1 + 4)
+
+  def testBlobkey(self):
+    # TODO(michaelcupino): Find out if scapes is using blobkey property
+    self.assertEqual(5, 1 + 4)
+
+  def testWordcountLink(self):
+    # TODO(michaelcupino): Find out if scapes is using wordcount_link property
+    self.assertEqual(5, 1 + 4)
+
+  def testIndexLink(self):
+    # TODO(michaelcupino): Find out if scapes is using index_link property
+    self.assertEqual(5, 1 + 4)
+
+  def testPhrasesLink(self):
+    # TODO(michaelcupino): Find out if scapes is using phrases_link property
+    self.assertEqual(5, 1 + 4)
+
+if __name__ == '__main__':
+  unittest.main()
+
