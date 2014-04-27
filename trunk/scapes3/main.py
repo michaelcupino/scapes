743110b132744eaed57fd4389c74c9948a8d060d
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-27 15:47:59 +0000

    Move ScapesStoreOutput pipeline into the pipeline package and add stubs for unit tests

--- trunk/scapes3/main.py
+++ trunk/scapes3/main.py
@@ -100,21 +100,3 @@ def scapes_analyze_revision(http, file_id, rev_id):
         for w in split_into_words(s.lower()):
             yield (w, "")
 
-class ScapesStoreOutput(base_handler.PipelineBase):
-  """A pipeline to store the result of the MapReduce job in the database.
-
-  Args:
-    mr_type: the type of mapreduce job run (e.g., WordCount, Index)
-    encoded_key: the DB key corresponding to the metadata of this job
-    output: the blobstore location where the output of the job is stored
-  """
-  
-  def run(self, mr_type, encoded_key, output):
-    logging.debug("output is %s" % str(output))
-    key = db.Key(encoded=encoded_key)
-    m = FileMetadata.get(key)
-    
-    if mr_type == "scapes_analyze":
-      m.wordcount_link = output[0]
-
-    m.put()
