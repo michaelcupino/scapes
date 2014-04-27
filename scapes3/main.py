679ab54264fbf8514d6bd965c61db3822cc1c6e3
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-27 16:45:59 +0000

    Change indentation in main.py.

--- scapes3/main.py
+++ scapes3/main.py
@@ -9,10 +9,8 @@ import webapp2
 from handler.index_handler import IndexHandler
 from service import config
 
-app = webapp2.WSGIApplication(
-    [
-        ('/', IndexHandler),
-        (config.decorator.callback_path, config.decorator.callback_handler()),
-    ],
-    debug=True)
+app = webapp2.WSGIApplication([
+    ('/', IndexHandler),
+    (config.decorator.callback_path, config.decorator.callback_handler()),
+], debug=True)
 
