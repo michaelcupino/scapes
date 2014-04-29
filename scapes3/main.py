0a9c68f29723705a6e7ea51d3bf44d393e4e7d51
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-04-29 01:45:57 +0000

    Add a handler to demo "getting a list of documents"

--- scapes3/main.py
+++ scapes3/main.py
@@ -6,11 +6,13 @@ bit.ly/scapescode
 bit.ly/scapeseng"""
 
 import webapp2
+from handler.folder_prototype_handler import FolderPrototypeHandler
 from handler.index_handler import IndexHandler
 from service import config
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
+    (r'/folder/(.*)', FolderPrototypeHandler),
     (config.decorator.callback_path, config.decorator.callback_handler()),
 ], debug=True)
 
