da616de5e93ade007216ba5e41b891fd61ec973e
michaelcupino@gmail.com <michaelcupino@gmail.com@e44e0e71-cc7b-37ac-1abf-a2f9c40da516>
2014-07-03 04:55:35 +0000

    Issue 111810044: Send csv attachments. Proof of concept.

--- trunk/scapes3/pipeline/email_pipeline.py
+++ trunk/scapes3/pipeline/email_pipeline.py
@@ -1,3 +1,6 @@
+import csv
+import tempfile
+
 from google.appengine.api import mail
 from mapreduce import base_handler
 
@@ -11,10 +14,23 @@ class EmailPipeline(base_handler.PipelineBase):
   """
   
   def run(self, toEmail, subject, body):
+    # TODO(michaelcupino): Move csv transforms into a service.
+    csvFile = tempfile.TemporaryFile()
+    writer = csv.writer(csvFile)
+    values = [['Date', 'Time', 'Who in doc', 'Word count', 'Words added',
+        'Words deleted', 'Punct. cap', 'Words moved', 'Document ID',
+        'Document Name']]
+    writer.writerows(values)
+
+    csvFile.seek(0)
+    csvFileBytes = csvFile.read()
+    csvFile.close()
+
     message = mail.EmailMessage()
     message.sender = 'robot@scapes-uci.appspotmail.com'
     message.to = toEmail
     message.subject = subject
     message.body = str(body)
+    message.attachments = [('hello.csv', csvFileBytes)]
     message.send()
 
